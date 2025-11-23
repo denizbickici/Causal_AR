6eeca2f13ea604f11d41fdf9a31f4a6c980c59da
denizbickici <denizbickici@mailbox.org>
2025-11-23 19:42:20 +0100

    first commit

---
+++ train_lavila_causal.sh
@@ -0,0 +1,36 @@
+data="egtea"
+vae_latent_dim=64
+alpha=1
+beta=0.1
+gamma=0.1
+delta=1
+
+action_dim=106
+verb_dim=19
+noun_dim=53
+epoch=3000
+lr=1e-4
+
+
+
+python3 main_lavila_causal.py \
+--num_thread_reader=4 \
+--cudnn_benchmark=1 \
+--pin_memory \
+--checkpoint_dir=whl \
+--vae_latent_dim ${vae_latent_dim} \
+--batch_size=8 \
+--batch_size_val=8 \
+--seed=0 \
+--evaluate \
+--dataset ${data} \
+--action_dim ${action_dim} \
+--verb_dim ${verb_dim} \
+--noun_dim ${noun_dim} \
+--epochs ${epoch} \
+--lr ${lr} \
+--gpu 0 \
+--resume \
+
+# vae classification jointly training
+
